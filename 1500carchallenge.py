eb2d05ddbe2a7d86b5bc4d5724e26fb0d4261826
Nick Lima <Nick Lima>
2017-06-16 21:34:18 -0400

    details implementation complete. Next step - add to output

--- 1500carchallenge.py
+++ 1500carchallenge.py
@@ -6,21 +6,9 @@ import discord
 import pdb
 import random
 import requests
+import sys
 
-client = discord.Client()
-channel = discord.Object(id='273260866198175745')
 lastResults = None
-
-@client.event
-async def on_ready():
-    print('Logged in as: ' + client.user.name + ' with user id ' + client.user.id)
-    await(car_search())
-
-@client.event
-async def on_message(message):
-    if message.content.startswith('!challenge'):
-        print('Challenge mode.')
-        await car_search_loop()
 
 async def car_search_loop():
     while not client.is_closed:
@@ -28,17 +16,68 @@ async def car_search_loop():
 
         #Set Sleep Here
         randtime = random.random()
-        print('Sleeping for ' + str(1 + randtime) + 'hours.')
-        await asyncio.sleep(60 * 60 * (1 + randtime)) 
+        randmins = 30 + 30*randtime
+        print('Sleeping for ' + str(randmins) + ' minutes.')
+        await asyncio.sleep(60 * randmins) 
         
 async def car_search(car=''):
     global lastResults
+    
+    resultsWithDupes = get_results()
+    results = []
+    # Remove duplicates
+    for n in resultsWithDupes:
+        if n['name'] not in [d['name'] for d in results]:
+            results.append(n)
+
+    results.sort(key=lambda k : datetime.datetime.strptime(k['datetime'], '%Y-%m-%d %H:%M'), reverse = False)
+
+    narrowTo = 10
+    print (str(len(results)) + ' results found.')
+    print ('Narrowing down to ' + str(min(narrowTo, len(results))) + '.') 
+    del results[narrowTo:]
+    
+    '''
+    print('lastResults:' )
+    if lastResults is not None:
+        for result in lastResults:
+            print(str(result['name']))
+    print('results:')
+    if results is not None:
+        for result in results:
+            print(str(result['name']))
+    '''
+
+    #First run
+    if lastResults == None:
+        print('No lastResults. Writing to Discord.')
+        get_detailed_results(results[0]['url'])
+        for result in results:
+            desc = "{0} | {1} | {2} |<{3}>".format(result["datetime"],  result["price"], result["name"], result["url"])
+            await client.send_message(channel, desc)
+
+    elif lastResults == results:
+        print('No new cars found.')
+        await client.send_message(channel, 'No new cars found.')
+
+    else:
+        print('Found new results.')
+        get_detailed_results(results[0]['url'])
+        for result in results:
+            if  result not in lastResults:
+                print(result['name'])
+                desc = "{0} | {1} | {2} |<{3}>".format(result["datetime"],  result["price"], result["name"], result["url"])
+                await client.send_message(channel, desc)
+
+    lastResults = results
+
+def get_results():
     url_base = 'https://boston.craigslist.org/search/cta'
     query_car = [
-    #'audi', 'mercedes', 'bmw', 'lexus', #luxury
-    #'prelude', 'si', 'crx', 'cr-x', #honda
-    #'240sx', '300zx', #nissan
-    #'cobalt', 'camaro', 'corvette', 'ss',  #chevy
+    'audi', 'mercedes', 'bmw', 'lexus', #luxury
+    'prelude', 'si', 'crx', 'cr-x', #honda
+    '240sx', '300zx', #nissan
+    'cobalt', 'camaro', 'corvette', 'ss',  #chevy
     #'g5', 'g6' , 'firebird', #pontiac
     'rsx', 'integra', #acura
     'svt', 'mustang', 'sho', #ford
@@ -57,8 +96,6 @@ async def car_search(car=''):
     querystring += ' '
     querystring += ' '.join(query_other)
 
-    print(querystring)
-
     params = dict(
         query=querystring,
         bundleDuplicates=1, 
@@ -67,57 +104,60 @@ async def car_search(car=''):
         min_price=500, 
         max_price=1500
         )
-    print(params)
     rsp = requests.get(url_base, params=params)
     print(rsp.raise_for_status())
 
     html = BeautifulSoup(rsp.text, 'html.parser')
     cars = html.find_all('p', attrs={'class':'result-info'})
-    resultsWithDupes = []
+    results = []
     for car in cars:
-        first = car.find_all('a', attrs={'class':'result-title hdrlnk'})[0]
-        name = first.text
-        url = first.get('href')
+        hyperlink = car.find_all('a', attrs={'class':'result-title hdrlnk'})[0]
+        name = hyperlink.text
+        url = hyperlink.get('href')
+        if 'craigslist.org' not in url:
+            url = 'https://boston.craigslist.org' + url
         price = car.find_all('span', attrs={'class':'result-price'})[0].text
         time = car.find_all('time')[0].get('datetime')
-        resultsWithDupes.append({'name':name, 'price':price, 'url':url, 'datetime':time})
-    
-    results = []
-    for n in resultsWithDupes:
-        if n['name'] not in [d['name'] for d in results]:
-            results.append(n)
-    results.sort(key=lambda k : datetime.datetime.strptime(k['datetime'], '%Y-%m-%d %H:%M'), reverse = True)
+        results.append({'name':name, 'price':price, 'url':url, 'datetime':time})
 
-    narrowTo = 10
-    print (str(len(results)) + ' results found.')
-    print ('Narrowing down to ' + str(min(narrowTo, len(results))) + '.') 
-    del results[narrowTo:]
-    
-    print('lastResults:' )
-    if lastResults is not None:
-        for result in lastResults:
-            print(str(result))
-    print('results:')
-    if results is not None:
-        for result in results:
-            print(str(result))
+    return results
 
-    #First run
-    if lastResults == None:
-        print('No lastResults. Writing to Discord.')
-        for result in results:
-            desc = "{0} | {1} | {2} |<{3}>".format(result["datetime"],  result["price"], result["name"], result["url"])
-            await client.send_message(channel, desc)
+def get_detailed_results(url):
+    #print('detailed results:')
+    rsp = requests.get(url)
+    html = BeautifulSoup(rsp.text, 'html.parser')
 
-    elif lastResults == results:
-         await client.send_message(channel, 'No new cars found.')
-
-    else:
-        for result in results:
-            if  result not in lastResults:
-                desc = "{0} | {1} | {2} |<{3}>".format(result["datetime"],  result["price"], result["name"], result["url"])
-                await client.send_message(channel, desc)
-    lastResults = results
+    model = html.find_all('p', attrs={'class':'attrgroup'})[0].find_all('b')[0].text
+    details = [n for n in html.find_all('p', attrs={'class':'attrgroup'})[1].get_text().split('\n') if n]
+    info = {}
+    for x in details:
+        info_name = x[:x.find(':')]
+        info_result = x[x.find(':')+2:]
+        info[info_name] = info_result
+    #print(info)
+    return info
 
 
-client.run('MjMyMzEyOTk4NTIwMjkxMzI5.CtRtEw.yBBPYU6dL-pWGVH_rPF7O2Fy4io')
+client = discord.Client()
+channel = discord.Object(id='324377346683699200')
+
+
+@client.event
+async def on_ready():
+    print('Logged in as: ' + client.user.name + ' with user id ' + client.user.id)
+    #await(car_search())
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!challenge'):
+        print('Challenge mode.')
+        await car_search_loop()
+    if message.content.startswith('!search'):
+        await car_search()
+
+try:
+    client.run('MjMyMzEyOTk4NTIwMjkxMzI5.CtRtEw.yBBPYU6dL-pWGVH_rPF7O2Fy4io')
+except ConnectionResetError:
+    client.run('MjMyMzEyOTk4NTIwMjkxMzI5.CtRtEw.yBBPYU6dL-pWGVH_rPF7O2Fy4io')
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
